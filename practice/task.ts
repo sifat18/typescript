801dfcd011b86f817c57f69cbec963c41a5a2ffb
mdsifat <ibnsifat@gmail.com>
2023-05-07 17:49:18 +0600

    update

--- practice/task.ts
+++ practice/task.ts
@@ -6,6 +6,7 @@ const arrayToTupple: [number, string, string, boolean, string] = [
   false,
   "23",
 ];
+
 // task 2
 const findCommon = (arr1: number[], arr2: number[]):number[] => {
   const ar: number[] = [];
@@ -37,3 +38,26 @@ const findProduct:findProductFunc=(ar,key,value)=>{
     }
 }
 console.log(findProduct([{id:1,name:"product1",price:100,category:"cat1"},{id:2,name:"product2",price:200,category:"cat2"}],"price",100))
+
+// task 4
+
+type productTupple<T,U,V>=[T,U,V]
+const tuppleArray:productTupple<string,number,number>[]= [["product1",100,100],["product2",200,200],["product3",300,300]]
+
+type funcTotal=(x:productTupple<string,number,number>[])=>number
+
+const total:funcTotal=(x)=>{
+    let sum=0;
+    for(let i=0;i<x.length;i++){
+        sum+=x[i][1]
+    }
+    return sum
+}
+console.log('total',total(tuppleArray))
+
+// task 5
+type findEvenSum=(arr:number[])=>number
+const findSumEven:findEvenSum=(arr)=>{
+    return arr.reduce((acc,curr)=>curr%2===0?acc+curr:0,0)
+}
+console.log(findSumEven([1,2,3,4,5,6,7,8,9,10]))
